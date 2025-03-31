2df0a316a10ff72203b29832bc68e6dab1124213
Nantha001 <nxtwave@ip-10-2-130-184.ap-south-1.compute.internal>
2025-03-31 12:49:48 +0000

    v2

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,8 +1,9 @@
 import {Component} from 'react'
 import './index.css'
+import {nanoid} from 'nanoid'
 import TodoItem from '../TodoItem'
 
-const todosList = [
+const initialTodosList = [
   {
     id: 1,
     title: 'Book the ticket for today evening',
@@ -38,28 +39,107 @@ const todosList = [
 ]
 
 class SimpleTodos extends Component {
-  state = {initialTodosList: todosList}
+  state = {
+    todosList: initialTodosList.map(each => ({
+      ...each,
+      isEdit: false,
+      isCheck: false,
+    })),
+    userInput: '',
+  }
 
   deleteTodoList = id => {
-    const {initialTodosList} = this.state
-    const deleteTodo = initialTodosList.filter(each => each.id !== id)
+    const {todosList} = this.state
+    const deleteTodo = todosList.filter(each => each.id !== id)
 
-    this.setState({initialTodosList: deleteTodo})
+    this.setState({todosList: deleteTodo})
+  }
+
+  onChangeInput = e => {
+    this.setState({userInput: e.target.value})
+  }
+
+  onChangeEdit = id => {
+    this.setState(pre => ({
+      todosList: pre.todosList.map(each =>
+        each.id === id ? {...each, isEdit: true} : each,
+      ),
+    }))
+  }
+
+  onChangeSave = (id, value) => {
+    this.setState(pre => ({
+      todosList: pre.todosList.map(each =>
+        each.id === id ? {...each, title: value, isEdit: false} : each,
+      ),
+    }))
+  }
+
+  onChangeCheck = (id, e) => {
+    const {checked} = e.target
+    this.setState(pre => ({
+      todosList: pre.todosList.map(each =>
+        each.id === id ? {...each, isCheck: checked} : each,
+      ),
+    }))
+  }
+
+  onSubmitInput = e => {
+    e.preventDefault()
+    const {userInput} = this.state
+    const splitUserInput = userInput.split(' ')
+    const numberTodo = parseInt(splitUserInput[splitUserInput.length - 1])
+    const task = splitUserInput.slice(0, -1).join(' ')
+    if (task && typeof numberTodo === 'number' && numberTodo > 0) {
+      const newObject = Array.from({length: numberTodo}, () => ({
+        id: nanoid(),
+        title: task,
+        isEdit: false,
+        isCheck: false,
+      }))
+      this.setState(pre => ({
+        todosList: [...pre.todosList, ...newObject],
+        userInput: '',
+      }))
+    } else {
+      this.setState(pre => ({
+        todosList: [
+          ...pre.todosList,
+          {id: nanoid(), title: userInput, isEdit: false, isCheck: false},
+        ],
+        userInput: '',
+      }))
+    }
   }
 
   render() {
-    const {initialTodosList} = this.state
+    const {todosList, userInput} = this.state
     return (
       <div className="bg">
         <div className="card">
           <h1 className="heading">Simple Todos</h1>
+          <form onSubmit={this.onSubmitInput}>
+            <input
+              type="text"
+              placeholder="type..."
+              onChange={this.onChangeInput}
+              value={userInput}
+              className="form-input"
+            />
+            <button type="submit">Add</button>
+          </form>
           <ul>
-            {initialTodosList.map(each => (
+            {todosList.map(each => (
               <TodoItem
                 key={each.id}
                 id={each.id}
                 deleteTodoList={this.deleteTodoList}
                 title={each.title}
+                isEdit={each.isEdit}
+                isCheck={each.isCheck}
+                onChangeCheck={this.onChangeCheck}
+                onChangeEdit={this.onChangeEdit}
+                onChangeSave={this.onChangeSave}
               />
             ))}
           </ul>
@@ -68,4 +148,5 @@ class SimpleTodos extends Component {
     )
   }
 }
+
 export default SimpleTodos
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -2,20 +2,69 @@ import './index.css'
 import {Component} from 'react'
 
 class TodoItem extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {editTitle: props.title}
+  }
+
   onDelete = () => {
     const {deleteTodoList, id} = this.props
     deleteTodoList(id)
   }
 
-  render() {
-    const {title} = this.props
+  onChangeInput = e => {
+    this.setState({editTitle: e.target.value})
+  }
 
+  render() {
+    const {
+      title,
+      onChangeCheck,
+      onChangeEdit,
+      onChangeSave,
+      isEdit,
+      isCheck,
+      id,
+    } = this.props
+
+    const {editTitle} = this.state
     return (
       <li>
-        <p>{title}</p>
-        <button onClick={this.onDelete} type="button">
-          Delete
-        </button>
+        <div className="check-input-container">
+          <input
+            type="checkbox"
+            onChange={e => onChangeCheck(id, e)}
+            checked={isCheck}
+          />
+          {isEdit ? (
+            <input
+              type="text"
+              onChange={this.onChangeInput}
+              value={editTitle}
+              className="edit-input"
+            />
+          ) : (
+            <p style={{textDecoration: isCheck ? 'line-through' : null}}>
+              {title}
+            </p>
+          )}
+        </div>
+        <div className="btn-container">
+          {isEdit ? (
+            <button onClick={() => onChangeSave(id, editTitle)} type="button">
+              Save
+            </button>
+          ) : (
+            <button onClick={() => onChangeEdit(id)} type="button">
+              Edit
+            </button>
+          )}
+
+          <button onClick={this.onDelete} type="button">
+            Delete
+          </button>
+        </div>
       </li>
     )
   }
